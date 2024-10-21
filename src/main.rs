2c6f043e35762c210efe4e154b9289f90070a1a4
Ian-Blockmans <38735005+Ian-Blockmans@users.noreply.github.com>
2024-10-21 20:17:35 +0200

    no cmd window

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-//#![windows_subsystem = "windows"]
+#![windows_subsystem = "windows"]
 use core::str;
 use std::env;
 use iced::{settings, window, Background, Border, Color, Shadow, Size, Task, Theme};
